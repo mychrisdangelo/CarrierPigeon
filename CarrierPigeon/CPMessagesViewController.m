65ae0a08690c655bcf855c08d259046cb2cdf18f
Chris D'Angelo <cd2665@columbia.edu>
2014-03-03 14:59:11 -0500

    attempting accessory view with ComposeBar

--- CarrierPigeon/CPMessagesViewController.m
+++ CarrierPigeon/CPMessagesViewController.m
@@ -10,9 +10,28 @@
 
 @interface CPMessagesViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *composeViewContainer;
+@property (readonly, nonatomic) PHFComposeBarView *composeBarView;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
+
 @end
 
 @implementation CPMessagesViewController
+
+@synthesize composeBarView = _composeBarView;
+- (PHFComposeBarView *)composeBarView {
+    if (!_composeBarView) {
+        CGRect frame = self.composeViewContainer.frame;
+        _composeBarView = [[PHFComposeBarView alloc] initWithFrame:frame];
+        [_composeBarView setMaxCharCount:160];
+        [_composeBarView setMaxLinesCount:5];
+        [_composeBarView setPlaceholder:@"Type something..."];
+        [_composeBarView setUtilityButtonImage:[UIImage imageNamed:@"Camera"]];
+        [_composeBarView setDelegate:self];
+    }
+    
+    return _composeBarView;
+}
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -28,13 +47,92 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
     
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillToggle:)
+                                                 name:UIKeyboardWillShowNotification
+                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillToggle:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+#warning todo observe and destroy message additions
+    
+//    [self.composeViewContainer addSubview:self.composeBarView];
+    
+    [self.view addSubview:self.composeBarView];
+    self.composeBarView.delegate = self;
+    [self.composeViewContainer removeFromSuperview];
+    
     self.title = self.user.displayName;
+}
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillShowNotification
+                                                  object:nil];
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillHideNotification
+                                                  object:nil];
+}
+
+- (void)keyboardWillToggle:(NSNotification *)notification {
+    NSDictionary* userInfo = [notification userInfo];
+    NSTimeInterval duration;
+    UIViewAnimationCurve animationCurve;
+    CGRect startFrame;
+    CGRect endFrame;
+    [[userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] getValue:&duration];
+    [[userInfo objectForKey:UIKeyboardAnimationCurveUserInfoKey]    getValue:&animationCurve];
+    [[userInfo objectForKey:UIKeyboardFrameBeginUserInfoKey]        getValue:&startFrame];
+    [[userInfo objectForKey:UIKeyboardFrameEndUserInfoKey]          getValue:&endFrame];
+    
+    NSInteger signCorrection = 1;
+    if (startFrame.origin.y < 0 || startFrame.origin.x < 0 || endFrame.origin.y < 0 || endFrame.origin.x < 0)
+        signCorrection = -1;
+    
+    CGFloat widthChange  = (endFrame.origin.x - startFrame.origin.x) * signCorrection;
+    CGFloat heightChange = (endFrame.origin.y - startFrame.origin.y) * signCorrection;
+    
+    CGFloat sizeChange = UIInterfaceOrientationIsLandscape([self interfaceOrientation]) ? widthChange : heightChange;
+    
+    CGRect newContainerFrame = [self.tableView frame];
+    newContainerFrame.size.height += sizeChange;
+    
+    [UIView animateWithDuration:duration
+                          delay:0
+                        options:(animationCurve << 16)|UIViewAnimationOptionBeginFromCurrentState
+                     animations:^{
+                         [self.tableView setFrame:newContainerFrame];
+                     }
+                     completion:NULL];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - PHFComposeBarViewDelegate
+
+- (void)composeBarView:(PHFComposeBarView *)composeBarView
+   willChangeFromFrame:(CGRect)startFrame
+               toFrame:(CGRect)endFrame
+              duration:(NSTimeInterval)duration
+        animationCurve:(UIViewAnimationCurve)animationCurve
+{
+//    [self prependTextToTextView:[NSString stringWithFormat:@"Height changing by %d", (NSInteger)(endFrame.size.height - startFrame.size.height)]];
+//    UIEdgeInsets insets = UIEdgeInsetsMake(0.0f, 0.0f, endFrame.size.height, 0.0f);
+//    UITextView *textView = [self textView];
+//    [textView setContentInset:insets];
+//    [textView setScrollIndicatorInsets:insets];
+}
+
+- (void)composeBarView:(PHFComposeBarView *)composeBarView
+    didChangeFromFrame:(CGRect)startFrame
+               toFrame:(CGRect)endFrame
+{
+//    [self prependTextToTextView:@"Height changed"];
 }
 
 #pragma mark - TableViewDataSource
@@ -64,5 +162,24 @@
     
     return cell;
 }
+
+#pragma mark - Text view delegate methods
+
+- (BOOL)textViewShouldBeginEditing:(UITextView *)aTextView
+{
+    if (aTextView.inputAccessoryView == nil) {
+        self.composeBarView.textView.inputAccessoryView = self.composeBarView;
+    }
+
+    
+    return YES;
+}
+
+//- (BOOL)textViewShouldEndEditing:(UITextView *)aTextView {
+//    
+//    [aTextView resignFirstResponder];
+//    
+//    return YES;
+//}
 
 @end
