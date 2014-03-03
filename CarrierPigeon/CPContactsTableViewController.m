d853e193e20f124e355ab8ac43cf22ed9ec0d15b
Chris D'Angelo <cd2665@columbia.edu>
2014-03-03 01:15:30 -0500

    searchbar filters contact list

--- CarrierPigeon/CPContactsTableViewController.m
+++ CarrierPigeon/CPContactsTableViewController.m
@@ -5,7 +5,9 @@
 //  Created by Chris D'Angelo on 3/2/14.
 //  Copyright (c) 2014 ColumbiaMobileComputing. All rights reserved.
 //
-// Some Images in the storybarod come free frome: http://www.pixellove.com
+//  Sources:
+//  Some Images in the storybarod come free frome: http://www.pixellove.com
+//  http://stackoverflow.com/questions/4471289/how-to-filter-nsfetchedresultscontroller-coredata-with-uisearchdisplaycontroll
 
 #import "CPContactsTableViewController.h"
 #import "DDLog.h"
@@ -20,15 +22,86 @@ static const int ddLogLevel = LOG_LEVEL_VERBOSE;
 static const int ddLogLevel = LOG_LEVEL_INFO;
 #endif
 
-@interface CPContactsTableViewController () <NSFetchedResultsControllerDelegate>
+@interface CPContactsTableViewController () <NSFetchedResultsControllerDelegate, UISearchDisplayDelegate>
 
 @property (nonatomic, strong) NSFetchedResultsController *fetchedResultsController;
+@property (nonatomic, strong) NSFetchedResultsController *searchFetchedResultsController;
 
 @end
 
 @implementation CPContactsTableViewController
 
 @synthesize fetchedResultsController = _fetchedResultsController;
+@synthesize searchFetchedResultsController = _searchFetchedResultsController;
+
+- (NSFetchedResultsController *)newFetchedResultsControllerWithSearch:(NSString *)searchString
+{
+    CPAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
+    NSManagedObjectContext *moc = [appDelegate managedObjectContext_roster];
+    NSPredicate *filterPredicate;
+    
+    NSEntityDescription *entity = [NSEntityDescription entityForName:@"XMPPUserCoreDataStorageObject"
+                                              inManagedObjectContext:moc];
+    
+    NSSortDescriptor *sd1 = [[NSSortDescriptor alloc] initWithKey:@"sectionNum" ascending:YES];
+    NSSortDescriptor *sd2 = [[NSSortDescriptor alloc] initWithKey:@"displayName" ascending:YES];
+    
+    NSArray *sortDescriptors = [NSArray arrayWithObjects:sd1, sd2, nil];
+    
+    NSMutableArray *predicateArray = [NSMutableArray array];
+    if (searchString.length) {
+        // your search predicate(s) are added to this array
+        [predicateArray addObject:[NSPredicate predicateWithFormat:@"displayName CONTAINS[cd] %@", searchString]];
+        // finally add the filter predicate for this view
+        if (filterPredicate) {
+            filterPredicate = [NSCompoundPredicate andPredicateWithSubpredicates:[NSArray arrayWithObjects:filterPredicate, [NSCompoundPredicate orPredicateWithSubpredicates:predicateArray], nil]];
+        } else {
+            filterPredicate = [NSCompoundPredicate orPredicateWithSubpredicates:predicateArray];
+        }
+    }
+    
+    NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
+    [fetchRequest setPredicate:filterPredicate];
+    [fetchRequest setEntity:entity];
+    [fetchRequest setSortDescriptors:sortDescriptors];
+    [fetchRequest setFetchBatchSize:10];
+    
+    _fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
+                                                                    managedObjectContext:moc
+                                                                      sectionNameKeyPath:@"sectionNum"
+                                                                               cacheName:nil];
+    
+    
+    [_fetchedResultsController setDelegate:self];
+    
+    
+    NSError *error = nil;
+    if (![_fetchedResultsController performFetch:&error])
+    {
+        DDLogError(@"Error performing fetch: %@", error);
+    }
+	
+	return _fetchedResultsController;
+}
+
+- (NSFetchedResultsController *)fetchedResultsController
+{
+    if (_fetchedResultsController != nil) {
+        return _fetchedResultsController;
+    }
+    _fetchedResultsController = [self newFetchedResultsControllerWithSearch:nil];
+    return _fetchedResultsController;
+}
+
+- (NSFetchedResultsController *)searchFetchedResultsController
+{
+    if (_searchFetchedResultsController != nil) {
+        return _searchFetchedResultsController;
+    }
+    
+    _searchFetchedResultsController = [self newFetchedResultsControllerWithSearch:self.searchDisplayController.searchBar.text];
+    return _searchFetchedResultsController;
+}
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -50,8 +123,6 @@ static const int ddLogLevel = LOG_LEVEL_INFO;
     // self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
 
-
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -66,6 +137,19 @@ static const int ddLogLevel = LOG_LEVEL_INFO;
             cpadfvc.delegate = self;
         }
     }
+}
+
+- (NSFetchedResultsController *)fetchedResultsControllerForTableView:(UITableView *)tableView
+{
+    return tableView == self.tableView ? self.fetchedResultsController : self.searchFetchedResultsController;
+}
+
+- (void)fetchedResultsController:(NSFetchedResultsController *)fetchedResultsController configureCell:(UITableViewCell *)theCell atIndexPath:(NSIndexPath *)theIndexPath
+{
+    XMPPUserCoreDataStorageObject *user = [fetchedResultsController objectAtIndexPath:theIndexPath];
+	
+	theCell.textLabel.text = user.displayName;
+	[self configurePhotoForCell:theCell user:user];
 }
 
 #pragma mark - CPAddFriendViewControllerDelegate
@@ -88,20 +172,20 @@ static const int ddLogLevel = LOG_LEVEL_INFO;
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    return [[self.fetchedResultsController sections] count];
+    return [[[self fetchedResultsControllerForTableView:tableView] sections] count];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-	NSArray *sections = [self.fetchedResultsController sections];
-	
-	if (section < [sections count])
-	{
-		id <NSFetchedResultsSectionInfo> sectionInfo = [sections objectAtIndex:section];
-		return sectionInfo.numberOfObjects;
-	}
-	
-	return 0;
+    NSInteger numberOfRows = 0;
+    NSFetchedResultsController *fetchController = [self fetchedResultsControllerForTableView:tableView];
+    NSArray *sections = fetchController.sections;
+    if(sections.count > 0) {
+        id <NSFetchedResultsSectionInfo> sectionInfo = [sections objectAtIndex:section];
+        numberOfRows = [sectionInfo numberOfObjects];
+    }
+    
+    return numberOfRows;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
@@ -109,16 +193,14 @@ static const int ddLogLevel = LOG_LEVEL_INFO;
 	static NSString *CellIdentifier = @"Cell";
 	
 	UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-	if (cell == nil)
-	{
+	if (cell == nil) {
 		cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault
                                       reuseIdentifier:CellIdentifier];
 	}
 	
-	XMPPUserCoreDataStorageObject *user = [[self fetchedResultsController] objectAtIndexPath:indexPath];
-	
-	cell.textLabel.text = user.displayName;
-	[self configurePhotoForCell:cell user:user];
+    [self fetchedResultsController:[self fetchedResultsControllerForTableView:tableView] configureCell:cell atIndexPath:indexPath];
+    
+
 	
 	return cell;
 }
@@ -142,6 +224,13 @@ static const int ddLogLevel = LOG_LEVEL_INFO;
 		else
 			cell.imageView.image = [UIImage imageNamed:@"defaultPerson"];
 	}
+}
+
+- (void)filterContentForSearchText:(NSString*)searchText scope:(NSInteger)scope
+{
+    // update the filter, in this case just blow away the FRC and let lazy evaluation create another with the relevant search info
+    self.searchFetchedResultsController.delegate = nil;
+    self.searchFetchedResultsController = nil;
 }
 
 /*
@@ -195,51 +284,90 @@ static const int ddLogLevel = LOG_LEVEL_INFO;
 
  */
 
-#pragma mark - NSFetchedResultsControllerDelegater
+#pragma mark - UISearchDisplayDelegate
 
-- (NSFetchedResultsController *)fetchedResultsController
+- (void)searchDisplayController:(UISearchDisplayController *)controller willUnloadSearchResultsTableView:(UITableView *)tableView;
 {
-	if (_fetchedResultsController == nil)
-	{
-        CPAppDelegate *appDelegate = [[UIApplication sharedApplication] delegate];
-		NSManagedObjectContext *moc = [appDelegate managedObjectContext_roster];
-		
-		NSEntityDescription *entity = [NSEntityDescription entityForName:@"XMPPUserCoreDataStorageObject"
-		                                          inManagedObjectContext:moc];
-		
-		NSSortDescriptor *sd1 = [[NSSortDescriptor alloc] initWithKey:@"sectionNum" ascending:YES];
-		NSSortDescriptor *sd2 = [[NSSortDescriptor alloc] initWithKey:@"displayName" ascending:YES];
-		
-		NSArray *sortDescriptors = [NSArray arrayWithObjects:sd1, sd2, nil];
-		
-		NSFetchRequest *fetchRequest = [[NSFetchRequest alloc] init];
-		[fetchRequest setEntity:entity];
-		[fetchRequest setSortDescriptors:sortDescriptors];
-		[fetchRequest setFetchBatchSize:10];
-		
-		_fetchedResultsController = [[NSFetchedResultsController alloc] initWithFetchRequest:fetchRequest
-                                                                            managedObjectContext:moc
-                                                                              sectionNameKeyPath:@"sectionNum"
-                                                                                       cacheName:nil];
-        
-        
-		[_fetchedResultsController setDelegate:self];
-		
-		
-		NSError *error = nil;
-		if (![_fetchedResultsController performFetch:&error])
-		{
-			DDLogError(@"Error performing fetch: %@", error);
-		}
-        
-	}
-	
-	return _fetchedResultsController;
+    // search is done so get rid of the search FRC and reclaim memory
+    self.searchFetchedResultsController.delegate = nil;
+    self.searchFetchedResultsController = nil;
 }
+
+- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchString:(NSString *)searchString
+{
+    [self filterContentForSearchText:searchString
+                               scope:[self.searchDisplayController.searchBar selectedScopeButtonIndex]];
+    
+    // Return YES to cause the search result table view to be reloaded.
+    return YES;
+}
+
+
+- (BOOL)searchDisplayController:(UISearchDisplayController *)controller shouldReloadTableForSearchScope:(NSInteger)searchOption
+{
+    [self filterContentForSearchText:[self.searchDisplayController.searchBar text]
+                               scope:[self.searchDisplayController.searchBar selectedScopeButtonIndex]];
+    
+    // Return YES to cause the search result table view to be reloaded.
+    return YES;
+}
+
+#pragma mark - NSFetchedResultsControllerDelegate
+
+- (void)controllerWillChangeContent:(NSFetchedResultsController *)controller
+{
+    UITableView *tableView = controller == self.fetchedResultsController ? self.tableView : self.searchDisplayController.searchResultsTableView;
+    [tableView beginUpdates];
+}
+
+- (void)controller:(NSFetchedResultsController *)controller
+  didChangeSection:(id <NSFetchedResultsSectionInfo>)sectionInfo
+           atIndex:(NSUInteger)sectionIndex
+     forChangeType:(NSFetchedResultsChangeType)type
+{
+    UITableView *tableView = controller == self.fetchedResultsController ? self.tableView : self.searchDisplayController.searchResultsTableView;
+    
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            [tableView insertSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+        case NSFetchedResultsChangeDelete:
+            [tableView deleteSections:[NSIndexSet indexSetWithIndex:sectionIndex] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
+- (void)controller:(NSFetchedResultsController *)controller
+   didChangeObject:(id)anObject
+       atIndexPath:(NSIndexPath *)theIndexPath
+     forChangeType:(NSFetchedResultsChangeType)type
+      newIndexPath:(NSIndexPath *)newIndexPath
+{
+    UITableView *tableView = controller == self.fetchedResultsController ? self.tableView : self.searchDisplayController.searchResultsTableView;
+    
+    switch(type) {
+        case NSFetchedResultsChangeInsert:
+            [tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+        case NSFetchedResultsChangeDelete:
+            [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:theIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+        case NSFetchedResultsChangeUpdate:
+            [self fetchedResultsController:controller configureCell:[tableView cellForRowAtIndexPath:theIndexPath] atIndexPath:theIndexPath];
+            break;
+        case NSFetchedResultsChangeMove:
+            [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:theIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            [tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newIndexPath] withRowAnimation:UITableViewRowAnimationFade];
+            break;
+    }
+}
+
 
 - (void)controllerDidChangeContent:(NSFetchedResultsController *)controller
 {
-	[[self tableView] reloadData];
+    UITableView *tableView = controller == self.fetchedResultsController ? self.tableView : self.searchDisplayController.searchResultsTableView;
+    [tableView endUpdates];
 }
+
 
 @end
