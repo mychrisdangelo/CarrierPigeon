29468c659857abddc271af4668a6cf9dea29d718
Chris D'Angelo <cd2665@columbia.edu>
2014-03-17 23:19:56 -0400

    added primitive online network status indicator

--- CarrierPigeon/CPSharingTableViewController.m
+++ CarrierPigeon/CPSharingTableViewController.m
@@ -7,9 +7,12 @@
 //
 
 #import "CPSharingTableViewController.h"
+#import "CPAppDelegate.h"
+#import "XMPP.h"
 
 @interface CPSharingTableViewController ()
 @property (weak, nonatomic) IBOutlet UITableViewCell *networkStatus;
+@property (nonatomic, strong) XMPPStream *xmppStream;
 
 @end
 
@@ -28,8 +31,25 @@
 {
     [super viewDidLoad];
     
-    self.networkStatus.imageView.image = [UIImage imageNamed:@"GreenCircle"];
+    CPAppDelegate *delegate = (CPAppDelegate *)[UIApplication sharedApplication].delegate;
+    self.xmppStream = delegate.xmppStream;
+    [self.xmppStream addDelegate:self delegateQueue:dispatch_get_main_queue()];
     
+    [self refreshDisplayOfNetworkStatus];
+}
+
+- (void)refreshDisplayOfNetworkStatus
+{
+    if ([self.xmppStream isConnected]) {
+        self.networkStatus.imageView.image = [UIImage imageNamed:@"GreenCircle"];
+        self.networkStatus.textLabel.text = @"Connected";
+    } else {
+        self.networkStatus.imageView.image = [UIImage imageNamed:@"RedCircle"];
+        self.networkStatus.textLabel.text = @"No network connection";
+        // todo: handle case of @"YellowCircle" when there is no network but there is a connection to other users
+    }
+    
+    [self.tableView reloadRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:0 inSection:0]] withRowAnimation:UITableViewRowAnimationNone];
 }
 
 - (void)didReceiveMemoryWarning
@@ -37,5 +57,18 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+#pragma mark - XMPPStreamDelegate
+
+- (void)xmppStreamDidAuthenticate:(XMPPStream *)sender
+{
+    [self refreshDisplayOfNetworkStatus];
+}
+
+- (void)xmppStreamDidDisconnect:(XMPPStream *)sender withError:(NSError *)error
+{
+    [self refreshDisplayOfNetworkStatus];
+}
+
 
 @end
